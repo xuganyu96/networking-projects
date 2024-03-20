4bd23a8e84ca5265bd2f70f9130c6f21b3c853c2
Ganyu (Bruce) Xu <xuganyu@berkeley.edu>
2024-03-19 21:47:42 -0400

    Organized code; display for Record

---
+++ handshake/src/codec.rs
@@ -0,0 +1,14 @@
+//! The Codec trait
+use std::io::Write;
+
+/// Data structure can be written onto a buffer and be extracted from a buffer
+pub trait Codec {
+    type Data: Sized;
+
+    /// Write the byte representation of self onto the writer
+    fn encode(&self, buffer: &mut impl Write) -> std::io::Result<usize>;
+
+    /// Read from the beginning of the buffer. If a piece of data can be parsed, return the piece
+    /// of data and the remainder of the buffer.
+    fn extract(buffer: &[u8]) -> Option<(Self::Data, &[u8])>;
+}
